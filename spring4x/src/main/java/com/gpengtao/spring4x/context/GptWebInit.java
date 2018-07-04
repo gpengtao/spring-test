5615b539e65a54a4e3dd0169c79013437a7fa2bc
耿鹏涛 <pengtao.geng@wormpex.com>
2018-07-04 12:35:28 +0800

    xiugai

--- spring4x/src/main/java/com/gpengtao/spring4x/context/GptWebInit.java
+++ spring4x/src/main/java/com/gpengtao/spring4x/context/GptWebInit.java
@@ -1,6 +1,3 @@
-/*
- * Copyright (c) 2018 Wormpex.com. All Rights Reserved.
- */
 package com.gpengtao.spring4x.context;
 
 /**
---
+++ spring4x/src/main/java/com/gpengtao/spring4x/event/GptEventListener.java
@@ -0,0 +1,8 @@
+
+package com.gpengtao.spring4x.event;
+
+/**
+ * @author pengtao.geng on 2018/7/4 上午11:31.
+ */
+public class GptEventListener {
+}
