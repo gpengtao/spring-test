d7905aeb23447fb5f09905b1f67699a48a50d8d8
pengtao.geng <pengtao.geng@bianlifeng.com>
2019-08-22 12:47:46 +0800

    gpt add

---
+++ test-spring-boot/src/main/java/com/gpengtao/testspringboot/TestSpringbootApplication.java
@@ -0,0 +1,23 @@
+package com.gpengtao.testspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+public class TestSpringbootApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		setRegisterErrorPageFilter(false);
+		return application.sources(TestSpringbootApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringbootApplication.class, args);
+	}
+
+}
